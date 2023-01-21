f1b21cdd9f882b6cb3cdf93125e0651e58ac4981
justchugh <snehalchugh2016@gmail.com>
2023-01-21 22:31:00 +0000

    commit

--- CODE/predictscores.py
+++ CODE/predictscores.py
@@ -1,71 +1,100 @@
 
-import discord
-from discord.ext import commands
-import random
+import numpy as np
+import pandas as pd
+import nltk
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
 
-bot_prefix = "!"
-client = commands.Bot(command_prefix=bot_prefix, case_insensitive=True)
+dataframe = pd.read_csv('newanswers.csv', index_col=False, header=None)
+x_train = dataframe[0].values
 
-command_usage = 0
+contractions = {
+    "ain't": "am not",
+    "aren't": "are not",
+    "can't": "cannot",
+    "couldn't": "could not",
+    "didn't": "did not",
+    "doesn't": "does not",
+    "don't": "do not",
+    "hadn't": "had not",
+    "hasn't": "has not",
+    "haven't": "have not",
+    "he'd": "he would",
+    "he'll": "he will",
+    "he's": "he is",
+    "I'd": "I would",
+    "I'll": "I will",
+    "I'm": "I am",
+    "I've": "I have",
+    "isn't": "is not",
+    "it's": "it is",
+    "let's": "let us",
+    "shouldn't": "should not",
+    "that's": "that is",
+    "there's": "there is",
+    "they'd": "they would",
+    "they'll": "they will",
+    "they're": "they are",
+    "they've": "they have",
+    "wasn't": "was not",
+    "we'd": "we would",
+    "we'll": "we will",
+    "we're": "we are",
+    "weren't": "were not",
+    "what's": "what is",
+    "where's": "where is",
+    "who's": "who is",
+    "won't": "will not",
+    "wouldn't": "would not",
+    "you'd": "you would",
+    "you'll": "you will",
+    "you're": "you are"
+}
 
-@client.event
-async def on_ready():
-    print(f"{client.user.name} is ready.")
-    await client.change_presence(activity=discord.Game(name=f"{client.command_prefix}help"))
+def expand_contractions(text):
+    words = text.split()
+    expanded_text = [contractions[word] if word in contractions else word for word in words]
+    return " ".join(expanded_text)
 
-@client.command(name="ping")
-async def ping(ctx):
-    global command_usage
-    await ctx.send(f"Pong! {round(client.latency * 1000)}ms")
-    command_usage += 1
+def clean_text(text):
+    lemmatizer = nltk.WordNetLemmatizer()
+    text = expand_contractions(text)
+    tokens = nltk.word_tokenize(text)
+    tokens = [word.lower() for word in tokens]
+    stop_words = set(nltk.corpus.stopwords.words('english')) - {'no', 'not', 'nor'}
+    filtered_tokens = [word for word in tokens if word.isalpha() and word not in stop_words]
+    lemmatized_tokens = [lemmatizer.lemmatize(word) for word in filtered_tokens]
+    return " ".join(lemmatized_tokens)
 
-@client.command(name="current_time")
-async def current_time(ctx):
-    global command_usage
-    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    await ctx.send(f"Current time: {now}")
-    command_usage += 1
+def prepare_texts(texts):
+    return [clean_text(text) for text in texts]
 
-@client.command(name="usage_count")
-async def usage_count(ctx):
-    global command_usage
-    await ctx.send(f"Commands used: {command_usage}")
-    command_usage += 1
+def vectorize_texts(train_texts, test_texts):
+    vectorizer = CountVectorizer(ngram_range=(1, 2))
+    train_vec = vectorizer.fit_transform(train_texts).toarray()
+    test_vec = vectorizer.transform(test_texts).toarray()
+    return test_vec
 
-@client.command(name="number_guess")
-async def number_guess(ctx):
-    global command_usage
-    secret_number = random.randint(1, 10)
-    await ctx.send("Guess a number between 1 and 10")
+def load_model(filename='finalized_model.sav'):
+    with open(filename, 'rb') as file:
+        model = pickle.load(file)
+    return model
 
-    def check_guess(msg):
-        return msg.author == ctx.author and msg.channel == ctx.channel and msg.content.isdigit()
+def predict_labels(test_vec):
+    model = load_model()
+    return model.predict(test_vec)
 
-    try:
-        guess = await client.wait_for("message", check=check_guess, timeout=15.0)
-        if int(guess.content) == secret_number:
-            await ctx.send("That's right!")
-        else:
-            await ctx.send(f"Nope! The number was {secret_number}")
-    except asyncio.TimeoutError:
-        await ctx.send("You didn't respond in time.")
+def generate_labels(test_texts):
+    clean_train_texts = prepare_texts(x_train)
+    clean_test_texts = prepare_texts(test_texts)
+    test_vectors = vectorize_texts(clean_train_texts, clean_test_texts)
+    predictions = predict_labels(test_vectors)
+    return predictions
 
-    command_usage += 1
-
-@client.command(name="help")
-async def help_command(ctx):
-    global command_usage
-    help_text = (
-        "!ping - Bot's latency
-"
-        "!current_time - Current time
-"
-        "!usage_count - Number of commands used
-"
-        "!number_guess - Guess the number game
-"
-    )
-    await ctx.send(help_text)
-    command_usage += 1
-
-client.run('YOUR_BOT_TOKEN')
+# Example usage:
+# x_test = ['I keep care of not leaving my belongings anywhere',
+#           'I try to not leave things anywhere',
+#           "I don't think strongly about this",
+#           'I am too lazy so I sometimes leave things around',
+#           'I always leave things around']
+# print(generate_labels(x_test))
